efdda2977e05bc99a13d4f42d77292f7508e25ae
Alexandru Iuga <alex.iuga@gmail.com>
2025-11-13 18:58:32 +0200

    updated the copyright information

--- sources/RequestR.Tests/RequestBusTests/UseCaseWithResponseTests.cs
+++ sources/RequestR.Tests/RequestBusTests/UseCaseWithResponseTests.cs
@@ -1,5 +1,5 @@
 // RequestR
-// Copyright (C) 2021 Dust in the Wind
+// Copyright (C) 2021-2025 Dust in the Wind
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
